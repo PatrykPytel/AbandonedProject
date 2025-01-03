8dfa8397d3adc1a3185665590f65ef119ebe8ee6
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2025-01-04 00:49:52 +0100

    fuck you

--- WillAbandonIt/Assets/Player/Script/simplePShooting.cs
+++ WillAbandonIt/Assets/Player/Script/simplePShooting.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class simplePShooting : MonoBehaviour
 {
-    private float cooldown = 0.1f;
+    public float cooldown = 0.1f;
     private float restartcooldown;
     public GameObject bullet;
     public Transform firePoint;
