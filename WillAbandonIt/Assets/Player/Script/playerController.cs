1e72090339ecba169677589114699ca43f625f04
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2025-01-03 02:30:53 +0100

    dsa

--- WillAbandonIt/Assets/Player/Script/playerController.cs
+++ WillAbandonIt/Assets/Player/Script/playerController.cs
@@ -10,6 +10,10 @@ public class playerController : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movementDirection;
     private Vector3 velocity = Vector3.zero;
+    public float cooldown = 1f;
+    private float oldcooldown;
+    private float dashtime = 0.5f;
+    private float mnoznik = 3f;
 
     // Start is called before the first frame update
     private void Awake()
@@ -18,16 +22,41 @@ public class playerController : MonoBehaviour
     }
     void Start()
     {
-        
+        oldcooldown = cooldown;
+        cooldown = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         movementDirection = new Vector2( Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) ;
+        if (cooldown <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                cooldown = oldcooldown;
+                //   animator.SetBool("Isdashing", true);
+                // animator.SetBool("IsJumping", false);
+                movementSpeed = movementSpeed * mnoznik;
+                Invoke("stopdash", dashtime);
+
+            }
+        }
+        else
+        {
+            cooldown -= Time.deltaTime;
+        }
     }
     private void FixedUpdate()
     {
         rb.velocity = Vector3.SmoothDamp(rb.velocity, movementDirection * movementSpeed, ref velocity, movementSmoothing);
+
     }//m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+    void stopdash()
+    {
+        movementSpeed = movementSpeed/ mnoznik;
+     //   animator.SetBool("Isdashing", false);
+        //       animator.SetBool("IsJumping",true);
+
+    }
 }
